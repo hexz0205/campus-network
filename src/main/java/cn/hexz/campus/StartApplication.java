8d76db3587477889834bcc58a29508de7d98c000
hexz0205 <woderuirui123>
2018-11-19 20:59:44 +0800

    启动类构建

--- src/main/java/cn/hexz/campus/StartApplication.java
+++ src/main/java/cn/hexz/campus/StartApplication.java
@@ -1,8 +1,12 @@
+package cn.hexz.campus;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 
 @SpringBootConfiguration
+@EnableAutoConfiguration
 public class StartApplication {
 
     public static void main(String[] args) {
