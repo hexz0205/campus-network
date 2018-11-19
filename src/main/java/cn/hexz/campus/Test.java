45e1094aa974870cee741e6a80587e101c9e4649
hexz0205 <woderuirui123>
2018-11-19 22:13:59 +0800

    测试类引入swagger文档

--- src/main/java/cn/hexz/campus/Test.java
+++ src/main/java/cn/hexz/campus/Test.java
@@ -1,12 +1,18 @@
 package cn.hexz.campus;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(description = "测试类")
+@RequestMapping("/root")
 public class Test {
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
+    @ApiOperation(value = "测试启动方法",notes = "测试启动方法")
     public String test() {
         System.out.println("springboot启动成功！");
         return "springboot启动成功！";
