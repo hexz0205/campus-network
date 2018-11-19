03e7a37114e9179aeb248c62ae78032c949e7cb6
hexz0205 <1765800227@qq.com>
2018-11-19 18:06:01 +0800

    项目构建

--- src/main/java/cn/hexz/campus/Test.java
+++ src/main/java/cn/hexz/campus/Test.java
@@ -1,4 +1,14 @@
 package cn.hexz.campus;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Test {
+
+    @RequestMapping("/test")
+    public String test() {
+        System.out.println("springboot启动成功！");
+        return "springboot启动成功！";
+    }
 }
